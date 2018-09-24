1de04336320bd65d4488ef6b50f261f86f7291fc
Rebekah Liu <pinkishy@gmail.com>
2018-09-24 23:29:06 +0000

    Completed Vigenere!

--- pset2/vigenere/vigenere.c
+++ pset2/vigenere/vigenere.c
@@ -11,7 +11,7 @@ int main(int argc, string argv[])
     int v; // new converted key
 
 
-// check no. or arguments given make sure there is only 1
+// check no. of arguments given make sure there is only 1
     if (argc != 2)
     {
         printf("Usage: ./caesar k\n");
@@ -19,7 +19,25 @@ int main(int argc, string argv[])
         return 1;
     }
 
+    else
+    {
+        //Check that key is in the alphabet
+        for (int i = 0, n = strlen(argv[1]); i < n; i++)
+        {
+            if (isalpha(argv[1][i])) // check through individual chars in the key is in the alphabet
+            {
+            }
+            else
+            {
+                printf("Usage: ./caesar k\n");
+                return 1;
+            }
+        }
+    }
+
     string k = argv[1]; // get the ceasar KEY value convert into integar k[i] is each letter
+
+
 
     string s = get_string("plaintext: "); // get text
     printf("ciphertext: "); // print out cipher
@@ -27,46 +45,54 @@ int main(int argc, string argv[])
 // iterate through plain text letter by letter
 
 
-    for (int i = 0, n = strlen(s); i < n; i++) // iterate through each char of string
+
+
+    for (int i = 0, n = strlen(s), count = 0 ; i < n; i++) // iterate through each char of string
     {
 
 
-// once position of key found. we convert into basic alphabetical order. THEN we add this on
-         l =  i % strlen(k); // l is the position in the key
+        l =  count % strlen(k); // l is the position in the key
 
-    if (islower(k[l]))
-            {
-                v = k[l] - 'a'; // v is the new converted key
-                            //   printf("%d", v);
-            }
+        if (islower(k[l]))
+        {
+            v = k[l] - 'a'; // v is the new converted key
+            // printf("%i", v);
+        }
 
         else if (isupper(k[l]))
-            {
-              v = k[l] - 'A'; // 75 - 65 = 10
-            //   printf("%i", v);
-            }
+        {
+            v = k[l] - 'A'; // 75 - 65 = 10
+            // printf("%i", v);
+        }
 
 
-            if (islower(s[i]))
-            {
-                printf("%c", (((s[i] - 'a') + v) % 26) + 'a'); // print out lowercase with key;
 
-            }
-            else if (isupper(s[i]))
-            {
-                printf("%c", (((s[i] - 'A') + v) % 26) + 'A');  // print out uppercase with key;
-            }
+        if (islower(s[i]))
+        {
+            printf("%c", (((s[i] - 'a') + v) % 26) + 'a'); // print out lowercase with key;
+            count++;
 
-            else
+        }
+        else if (isupper(s[i]))
+        {
+            printf("%c", (((s[i] - 'A') + v) % 26) + 'A');  // print out uppercase with key;
+            count++;
+        }
 
-            {
-                // l =  (i - 1) % strlen(k);
-                printf("%c", s[i]);
-                return 0;
+        else
 
-            }
+        {
+            printf("%c", s[i]);
 
+// printf("%i", v);
+        }
 
     }
+
+
+    printf("\n");
+    return 0;
+
+
 
 }
